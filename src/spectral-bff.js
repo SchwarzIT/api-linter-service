2223bd18c8d896160839c3594215c2097ac9a83b
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2022-02-22 17:21:08 +0100

    Downloaded the latest spectral rules

--- src/spectral-bff.js
+++ src/spectral-bff.js
@@ -1,4 +1,4 @@
-const {length: length$0, pattern, truthy} = require("@stoplight/spectral-functions");
+const {length: length$0, pattern, truthy, xor} = require("@stoplight/spectral-functions");
 const {oas} = require("@stoplight/spectral-rulesets");
 module.exports = {
   "extends": [{
@@ -6,7 +6,7 @@ module.exports = {
     "rules": {
       "operation-tag-defined": "off",
       "path-must-match-api-standards": {
-        "description": "API Path must match company api uri standards",
+        "description": "API Path must match company API uri standards",
         "message": "{{description}}; {{property}} incorrect. Example: /digital-twin/api/v1/products",
         "severity": "error",
         "resolved": false,
@@ -19,7 +19,7 @@ module.exports = {
         }
       },
       "servers-must-match-api-standards": {
-        "description": "Servers must match company api standards",
+        "description": "Schema and host in URL must match company API standards",
         "message": "{{description}}; {{property}}:{{value}} incorrect. Example: https://live.api.schwarz/digital-twin/api/v1/products",
         "severity": "error",
         "resolved": false,
@@ -27,9 +27,66 @@ module.exports = {
         "then": {
           "function": pattern,
           "functionOptions": {
-            "match": "^((http[s]?)://)([a-z]+)([.+])api.schwarz"
+            "match": "^((http[s]?):\\/\\/)([a-z]+)([.+])api.schwarz"
           }
         }
+      },
+      "info-description": {
+        "description": "Every API must have a global description",
+        "message": "OpenAPI object info `description` must be present and at least 100 chars long.",
+        "severity": "error",
+        "given": "$.info",
+        "then": [{
+          "field": "description",
+          "function": truthy
+        }, {
+          "field": "description",
+          "function": length$0,
+          "functionOptions": {
+            "min": 100
+          }
+        }]
+      },
+      "contact-information": {
+        "description": "Every API must have a contact containing name, email and a url",
+        "message": "{{description}}; property {{property}} is missing",
+        "severity": "error",
+        "given": "$.info.contact",
+        "then": [{
+          "field": "name",
+          "function": truthy
+        }, {
+          "field": "email",
+          "function": truthy
+        }, {
+          "field": "url",
+          "function": truthy
+        }]
+      },
+      "path-dto-reference": {
+        "description": "DTOs should be used to specify the schema(data types) of a request / response",
+        "message": "{{description}}; property {{property}} is missing",
+        "severity": "error",
+        "given": "$.components.schemas",
+        "then": [{
+          "function": length$0,
+          "functionOptions": {
+            "min": 1
+          }
+        }]
+      },
+      "must-have-path": {
+        "description": "Every API must have at least one path",
+        "message": "{{description}}; property `paths` is empty",
+        "severity": "error",
+        "given": "$",
+        "then": [{
+          "field": "paths",
+          "function": length$0,
+          "functionOptions": {
+            "min": 1
+          }
+        }]
       },
       "common-responses-unauthorized": {
         "description": "Responses should contain common response - 401 (unauthorized)",
@@ -64,18 +121,18 @@ module.exports = {
         }]
       },
       "no-http-verbs-in-resources": {
-        "description": "The HTTP URL should not be used to define different actions on a resource",
+        "description": "The HTTP Verbs should not be used in the route path to define different actions on a resource",
         "message": "{{description}}; {{property}} Instead use HTTP verbs to define actions on a resource. Example: PUT - /digital-twin/api/v1/products/42",
         "severity": "error",
         "given": "$.paths[?(!@property.match(/well-known/ig))]~",
         "then": {
           "function": pattern,
           "functionOptions": {
-            "notMatch": "/(get|post|put|patch|delete)(/|$)"
+            "notMatch": "\\/(get|post|put|patch|delete)(\\/|$)"
           }
         }
       },
-      "description-is-mandatory": {
+      "path-description-is-mandatory": {
         "description": "Every route of an API should have a description",
         "message": "{{description}}; property: {{property}} is missing",
         "severity": "error",
@@ -85,54 +142,51 @@ module.exports = {
           "function": truthy
         }]
       },
-      "info-description": {
-        "description": "Every API must have a global description",
-        "message": "OpenAPI object info `description` must be present and at least 100 chars long.",
+      "must-have-response-body": {
+        "description": "Every route returning a http status code of 200 or 201 must have a response body defined",
+        "message": "{{description}}; property {{property}} is missing at path: {{path}}",
         "severity": "error",
-        "given": "$.info",
+        "given": "$.paths[?(!@property.match(/well-known/ig))]..responses[200,201]",
         "then": [{
-          "field": "description",
+          "field": "content",
           "function": truthy
         }, {
           "field": "description",
-          "function": length$0,
+          "function": truthy
+        }]
+      },
+      "must-have-content-type": {
+        "description": "Every response must specify its content type",
+        "message": "{{description}}; property {{property}} is missing or not a valid content-type",
+        "severity": "error",
+        "given": "$.paths[?(!@property.match(/well-known/ig))]..content",
+        "then": [{
+          "field": "@key",
+          "function": pattern,
           "functionOptions": {
-            "min": 100
+            "match": "/"
           }
         }]
       },
-      "contact-information": {
-        "description": "Every API must have a contact containing name, email and a url",
-        "message": "{{description}}; property {{property}} is missing",
+      "must-define-example-schema": {
+        "description": "Every DTO must define at least one example",
+        "message": "{{description}}; DTO is lacking an example {{path}}",
         "severity": "error",
-        "given": "$.info.contact",
+        "given": "$.paths[?(!@property.match(/well-known/ig))]..content.*",
         "then": [{
-          "field": "name",
-          "function": truthy
-        }, {
-          "field": "email",
-          "function": truthy
-        }, {
-          "field": "url",
-          "function": truthy
+          "function": xor,
+          "functionOptions": {
+            "properties": ["example", "examples"]
+          }
         }]
       },
-      "path-description": {
-        "description": "Each path of an API must have a description",
-        "message": "{{description}}; property `description` is missing for path {{path}}",
+      "path-must-specify-tags": {
+        "description": "Every route must specify at least one tag it belongs to",
+        "message": "{{description}}; property tags is missing at: {{path}}",
         "severity": "error",
-        "given": "$.paths[?(!@property.match(/well-known/ig))]",
+        "given": "$.paths[?(!@property.match(/well-known/ig))].*",
         "then": [{
-          "field": "description",
-          "function": truthy
-        }]
-      },
-      "path-dto-reference": {
-        "description": "DTOs should be used to specify the schema of a request / response",
-        "message": "{{description}}; property {{property}} is missing",
-        "severity": "error",
-        "given": "$.components.schemas",
-        "then": [{
+          "field": "tags",
           "function": length$0,
           "functionOptions": {
             "min": 1
@@ -145,13 +199,17 @@ module.exports = {
     "operation-tag-defined": "off",
     "path-must-match-api-standards": "off",
     "servers-must-match-api-standards": "off",
-    "common-responses-unauthorized": "info",
+    "common-responses-unauthorized": "hint",
     "http-verbs-should-be-used": "info",
     "no-http-verbs-in-resources": "info",
-    "description-is-mandatory": "info",
+    "path-description-is-mandatory": "info",
     "info-description": "info",
     "contact-information": "info",
-    "path-description": "info",
-    "path-dto-reference": "info"
+    "path-dto-reference": "info",
+    "must-have-path": "info",
+    "must-have-response-body": "info",
+    "must-have-content-type": "info",
+    "must-define-example-schema": "info",
+    "path-must-specify-tags": "info"
   }
 };
