73be59c7ab70ab0d8af1779ce17d98319ff3553c
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2022-12-09 08:27:11 +0100

    chore: Update linter service with latest ruleset

--- src/spectral-legacy.js
+++ src/spectral-legacy.js
@@ -1,4 +1,4 @@
-const {oas2, oas3} = require("@stoplight/spectral-formats");
+const {oas3} = require("@stoplight/spectral-formats");
 const {length: length$0, pattern, truthy, xor} = require("@stoplight/spectral-functions");
 const {oas} = require("@stoplight/spectral-rulesets");
 module.exports = {
@@ -64,18 +64,6 @@ module.exports = {
           "function": truthy
         }]
       },
-      "path-dto-reference": {
-        "description": "DTOs should be used to specify the schema(data types) of a request / response",
-        "message": "{{description}}; property {{property}} is missing",
-        "severity": "error",
-        "given": "$.components.schemas",
-        "then": [{
-          "function": length$0,
-          "functionOptions": {
-            "min": 1
-          }
-        }]
-      },
       "must-have-path": {
         "description": "Every API must have at least one path",
         "message": "{{description}}; property `paths` is empty",
@@ -99,28 +87,6 @@ module.exports = {
           "function": truthy
         }]
       },
-      "http-verbs-should-be-used": {
-        "description": "HTTP verbs should be used to express different actions or functions on a resource",
-        "message": "{{description}}; The HTTP verb {{property}} is missing.",
-        "severity": "error",
-        "given": "$.paths[?(!@property.match(/well-known/ig))]",
-        "then": [{
-          "field": "get",
-          "function": truthy
-        }, {
-          "field": "post",
-          "function": truthy
-        }, {
-          "field": "put",
-          "function": truthy
-        }, {
-          "field": "patch",
-          "function": truthy
-        }, {
-          "field": "delete",
-          "function": truthy
-        }]
-      },
       "no-http-verbs-in-resources": {
         "description": "The HTTP Verbs should not be used in the route path to define different actions on a resource",
         "message": "{{description}}; {{property}} Instead use HTTP verbs to define actions on a resource. Example: PUT - /digital-twin/api/v1/products/42",
@@ -139,20 +105,6 @@ module.exports = {
         "severity": "error",
         "given": "$.paths[?(!@property.match(/well-known/ig))]",
         "then": [{
-          "field": "description",
-          "function": truthy
-        }],
-        "formats": [oas3]
-      },
-      "must-have-response-body": {
-        "description": "Every route returning a http status code of 200 or 201 must have a response body defined",
-        "message": "{{description}}; property {{property}} is missing at path: {{path}}",
-        "severity": "error",
-        "given": "$.paths[?(!@property.match(/well-known/ig))].[?(@property === 'get')].responses[200,201,\"200\",\"201\"]",
-        "then": [{
-          "field": "content",
-          "function": truthy
-        }, {
           "field": "description",
           "function": truthy
         }],
@@ -202,20 +154,6 @@ module.exports = {
           }
         }],
         "formats": [oas3]
-      },
-      "oas2-must-have-response-body": {
-        "description": "Every route returning a http status code of 200 or 201 must have a response body defined",
-        "message": "{{description}}; property {{property}} is missing at path: {{path}}",
-        "severity": "error",
-        "formats": [oas2],
-        "given": "$.paths[?(!@property.match(/well-known/ig))].[?(@property === 'get')].responses[200,201,\"200\",\"201\"]",
-        "then": [{
-          "field": "schema",
-          "function": truthy
-        }, {
-          "field": "description",
-          "function": truthy
-        }]
       }
     }
   }],
@@ -224,15 +162,11 @@ module.exports = {
     "path-must-match-api-standards": "off",
     "servers-must-match-api-standards": "off",
     "common-responses-unauthorized": "hint",
-    "http-verbs-should-be-used": "off",
     "no-http-verbs-in-resources": "info",
     "path-description-is-mandatory": "info",
     "info-description": "info",
     "contact-information": "info",
-    "path-dto-reference": "info",
     "must-have-path": "info",
-    "must-have-response-body": "info",
-    "oas2-must-have-response-body": "info",
     "must-have-content-type": "info",
     "must-define-example-schema": "info",
     "path-must-specify-tags": "info",
--- src/spectral.js
+++ src/spectral.js
@@ -1,4 +1,4 @@
-const {oas2, oas3} = require("@stoplight/spectral-formats");
+const {oas3} = require("@stoplight/spectral-formats");
 const {length: length$0, pattern, truthy, xor} = require("@stoplight/spectral-functions");
 const {oas} = require("@stoplight/spectral-rulesets");
 module.exports = {
@@ -63,18 +63,6 @@ module.exports = {
         "function": truthy
       }]
     },
-    "path-dto-reference": {
-      "description": "DTOs should be used to specify the schema(data types) of a request / response",
-      "message": "{{description}}; property {{property}} is missing",
-      "severity": "error",
-      "given": "$.components.schemas",
-      "then": [{
-        "function": length$0,
-        "functionOptions": {
-          "min": 1
-        }
-      }]
-    },
     "must-have-path": {
       "description": "Every API must have at least one path",
       "message": "{{description}}; property `paths` is empty",
@@ -98,28 +86,6 @@ module.exports = {
         "function": truthy
       }]
     },
-    "http-verbs-should-be-used": {
-      "description": "HTTP verbs should be used to express different actions or functions on a resource",
-      "message": "{{description}}; The HTTP verb {{property}} is missing.",
-      "severity": "error",
-      "given": "$.paths[?(!@property.match(/well-known/ig))]",
-      "then": [{
-        "field": "get",
-        "function": truthy
-      }, {
-        "field": "post",
-        "function": truthy
-      }, {
-        "field": "put",
-        "function": truthy
-      }, {
-        "field": "patch",
-        "function": truthy
-      }, {
-        "field": "delete",
-        "function": truthy
-      }]
-    },
     "no-http-verbs-in-resources": {
       "description": "The HTTP Verbs should not be used in the route path to define different actions on a resource",
       "message": "{{description}}; {{property}} Instead use HTTP verbs to define actions on a resource. Example: PUT - /digital-twin/api/v1/products/42",
@@ -138,20 +104,6 @@ module.exports = {
       "severity": "error",
       "given": "$.paths[?(!@property.match(/well-known/ig))]",
       "then": [{
-        "field": "description",
-        "function": truthy
-      }],
-      "formats": [oas3]
-    },
-    "must-have-response-body": {
-      "description": "Every route returning a http status code of 200 or 201 must have a response body defined",
-      "message": "{{description}}; property {{property}} is missing at path: {{path}}",
-      "severity": "error",
-      "given": "$.paths[?(!@property.match(/well-known/ig))].[?(@property === 'get')].responses[200,201,\"200\",\"201\"]",
-      "then": [{
-        "field": "content",
-        "function": truthy
-      }, {
         "field": "description",
         "function": truthy
       }],
@@ -201,20 +153,6 @@ module.exports = {
         }
       }],
       "formats": [oas3]
-    },
-    "oas2-must-have-response-body": {
-      "description": "Every route returning a http status code of 200 or 201 must have a response body defined",
-      "message": "{{description}}; property {{property}} is missing at path: {{path}}",
-      "severity": "error",
-      "formats": [oas2],
-      "given": "$.paths[?(!@property.match(/well-known/ig))].[?(@property === 'get')].responses[200,201,\"200\",\"201\"]",
-      "then": [{
-        "field": "schema",
-        "function": truthy
-      }, {
-        "field": "description",
-        "function": truthy
-      }]
     }
   }
 };
